bd442042b435977753c6b754c65fee37eda92eb0
Miguel Galvez <jmtgalvez@gmail.com>
2025-08-19 15:22:06 +0800

    styled homepage

--- web-app/src/pages/home.js
+++ web-app/src/pages/home.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <div className="page page-home">
+      <div className="page-header">
+        <img src={require("../images/zeniark-logo.png")} alt="logo" />
+      </div>
       <div className="page-title">
         <h1>Welcome to the Trivia Challenge!</h1>
       </div>
       <div className="page-body">
         <p>You will be presented with 10 True or False questions.</p>
-        <p>Can you score 10/10?</p>
+        <p className="btn">Can you score 10/10?</p>
       </div>
       <div className="page-control">
-        <button>
-          <Link to="quiz">LET’S START!</Link>
-        </button>
+        <Link to="quiz">LET’S START!</Link>
       </div>
     </div>
-  )
+  );
 }
